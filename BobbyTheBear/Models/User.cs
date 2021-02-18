de3f81ee2e4e99dbbef011bfa188aa6e94d023c2
Noah <noahy_1@msn.com>
2021-02-18 02:33:22 +0100

    initial
    initial

---
+++ BobbyTheBear/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BobbyTheBear.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DataType(DataType.Text)]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [NotMapped]
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
---
+++ BobbyTheBear/Models/UserDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BobbyTheBear.Models
+{
+    public class UserDbContext:DbContext
+    {
+
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
